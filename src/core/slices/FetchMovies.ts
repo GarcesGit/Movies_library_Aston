aa1370dd2046b6e8eb2bff2a5d5fa8e32c0b3eb6
Natalya Koshukova <koshukovanat@gmail.com>
2024-04-01 13:34:05 +0700

    .

--- src/core/slices/FetchMovies.ts
+++ src/core/slices/FetchMovies.ts
@@ -1,21 +1,13 @@
 import { Dispatch } from 'redux';
 import { FetchMoviesResponse } from '../../types/ReduxTypes/MovieType';
 import { movieSlice } from './MovieSlice';
-import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-
-// const API_key = `apikey=${process.env.REACT_APP_API_KEY}`;
-// const API_URL = `https://www.omdbapi.com/?${API_key}`
-// в работе
+import { API_URL } from '../../routes/routes';
 
 export const fetchMovies = () => async (dispatch: Dispatch) => {
   try {
     dispatch(movieSlice.actions.moviesFetching());
-    const response = await axios.get<FetchMoviesResponse>(
-      'https://www.omdbapi.com/?s=day&apikey=786d4402',
-      // `${API_URL}&s=day`,
-    );
-
+    const response = await axios.get<FetchMoviesResponse>(`${API_URL}&s=day`);
     dispatch(movieSlice.actions.moviesFetchingSuccess(response.data.Search));
   } catch (e: any) {
     dispatch(movieSlice.actions.moviesFetchingError(e.message));
